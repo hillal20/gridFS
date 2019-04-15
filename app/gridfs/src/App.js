bfd5acb298e8d45197e586ad8bc88279e9e8a4a5
hilal aissani <hilalaissani@gmail.com>
2019-04-15 19:47:03 -0400

    jando

--- app/gridfs/src/App.js
+++ app/gridfs/src/App.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: "",
+      dataBaseFile: ""
+    };
+  }
+  componentWillMount() {
+    this.sendFile();
+  }
+
+  eventHandler = event => {
+    this.setState({ file: event.target.files[0] });
+  };
+  sendFile = () => {
+    const data = new FormData();
+    data.append("image", this.state.file);
+    console.log("data===>", data.get("image"));
+    const promise = axios.post("http://localhost:4005/uploads", data, {
+      onUploadProgress: progressEvent => {
+        console.log(
+          "progress==>",
+          Math.round((progressEvent.loaded / progressEvent.total) * 100),
+          "%"
+        );
+      }
+    });
+    promise
+      .then(res => {
+        console.log("res====> ", res.data);
+        this.setState({ dataBaseFile: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   render() {
+    console.log("this.state.file===>", this.state.file);
+    console.log("this.state.dataBaseFile===>", this.state.dataBaseFile.file);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>upload your picture</h3>
+        <input type="file" name="file" onChange={this.eventHandler} />
+        <button onClick={this.sendFile}>send</button>
+        <img
+          src="http://localhost:4005/image/54ce993a7bb1bf4798ae62edb563a3ac.jpg"
+          width="400"
+        />
       </div>
     );
   }
--- app/server/server.js
+++ app/server/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const server = express();
 const mongoose = require("mongoose");
 const multer = require("multer");
-const gridFsStorage = require("multer-gridfs");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methode = require("gridfs-stream");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
@@ -12,6 +14,7 @@ const cons = require("consolidate");
 const compression = require("compression");
 const morgan = require("morgan");
 const { createServer } = require("http");
+const crypto = require("crypto");
 server.use(cors());
 server.use(helmet());
 server.use(bodyParser.json());
@@ -25,22 +28,100 @@ if (!dev) {
 }
 
 const port = process.env.PORT || 4005;
-mongoose
-  .connect(
-    process.env.MONGO_URL,
-    { useNewUrlParser: true }
-  )
-  .then(p => {
-    console.log("=== connected to lambdadaFinal==");
-  })
-  .catch(err => {
-    console.log(`err:${err}`);
-  });
-
-server.use(express.static(path.join(__dirname, "../gridfs/build")));
-server.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../gridfs/build", "index.html"));
+const connection = mongoose.createConnection(process.env.MONGO_URL, {
+  useNewUrlParser: true
 });
+// .then(p => {
+//   console.log("=== connected to lambdadaFinal==");
+// })
+// .catch(err => {
+//   console.log(`err:${err}`);
+// });
+////////////////////////////////////////////////////////
+let gfs;
+connection.once("open", () => {
+  gfs = Grid(connection.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+///////////////////////////////////// s
+const storage = new GridFsStorage({
+  url: process.env.MONGO_URL,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads"
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage: storage });
+
+server.post("/uploads", upload.single("image"), (req, res) => {
+  console.log("file ====>", req.file);
+  res.json({ file: req.file });
+});
+
+/////////////////////////////////////////////////
+server.get("/files", (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    if (!files || files.length === 0) {
+      res.status(404).json({ msg: "no files " });
+    }
+    return res.json(files);
+  });
+});
+
+//////////////////////////////////////////////
+
+server.get("/files/:filename", (req, res) => {
+  const filename = req.params.filename;
+  gfs.files
+    .findOne({ filename: filename })
+    .then(file => {
+      if (file) {
+        res.json(file);
+      }
+      res.json({ msg: "there is no file" });
+    })
+    .catch(err => {
+      res.json({ err: err });
+    });
+});
+///////////////////////////// desplay the image
+
+server.get("/image/:filename", (req, res) => {
+  const filename = req.params.filename;
+  gfs.files
+    .findOne({ filename: filename })
+    .then(file => {
+      if (
+        file.contentType === "image/jpeg" ||
+        file.contentType === "image/png"
+      ) {
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.json({ msg: "it is not an  image" });
+      }
+    })
+    .catch(err => {
+      res.json({ err: err });
+    });
+});
+
+// server.use(express.static(path.join(__dirname, "../gridfs/build")));
+// server.use("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../gridfs/build", "index.html"));
+// });
 
 const SafeServer = createServer(server);
 
